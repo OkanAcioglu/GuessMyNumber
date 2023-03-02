f2ea32593fc445ce0a80556f0e6ecaf5fa19872e
Okan <okanacioglu@gmail.com>
2023-03-02 20:00:42 +0300

    managing-colors-globally

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,4 +1,5 @@
 import { View, Text, Pressable, StyleSheet } from 'react-native'
+import Colors from '../screens/constants/colors'
 
 const PrimaryButton = ({ children, onPress }) => {
   // function pressHandler() {
@@ -14,7 +15,7 @@ const PrimaryButton = ({ children, onPress }) => {
             : styles.buttonInnerContainer
         }
         onPress={onPress}
-        android_ripple={{ color: '#640233' }}
+        android_ripple={{ color: Colors.primary600 }}
       >
         <Text style={styles.buttonText}>{children}</Text>
       </Pressable>
@@ -31,7 +32,7 @@ const styles = StyleSheet.create({
     overflow: 'hidden',
   },
   buttonInnerContainer: {
-    backgroundColor: '#72063c',
+    backgroundColor: Colors.primary500,
     paddingVertical: 8,
     paddingHorizontal: 16,
     elevation: 2,
