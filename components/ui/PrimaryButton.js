3e0846d560173debcabe928e4d9dd72deec78357
Okan <okanacioglu@gmail.com>
2023-03-02 20:37:54 +0300

    create-using-displaying-numbers

--- components/ui/PrimaryButton.js
+++ components/ui/PrimaryButton.js
@@ -1,5 +1,5 @@
 import { View, Text, Pressable, StyleSheet } from 'react-native'
-import Colors from '../screens/constants/colors'
+import Colors from '../../screens/constants/colors'
 
 const PrimaryButton = ({ children, onPress }) => {
   // function pressHandler() {
