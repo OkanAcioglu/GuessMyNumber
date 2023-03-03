ac1798029866e8e0a29967b1e5b0eb1113fd72ef
Okan <okanacioglu@gmail.com>
2023-03-03 21:04:45 +0300

    final-touches

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -2,10 +2,12 @@ import { View, StyleSheet, Alert, Text, FlatList } from 'react-native'
 import Title from '../components/ui/Title'
 import { useState, useEffect } from 'react'
 import NumberContainer from '../components/game/NumberContainer'
+import GuessLogItem from '../components/game/GuessLogItem'
 import PrimaryButton from '../components/ui/PrimaryButton'
 import Card from '../components/ui/Card'
 import InstructionText from '../components/ui/InstructionText'
 import { Ionicons } from '@expo/vector-icons'
+
 function generateRandomBetween(min, max, exclude) {
   const rndNum = Math.floor(Math.random() * (max - min)) + min
 
@@ -26,7 +28,7 @@ const GameScreen = ({ userNumber, onGameOver }) => {
 
   useEffect(() => {
     if (currentGuess === userNumber) {
-      onGameOver()
+      onGameOver(guessRounds.length)
     }
   }, [currentGuess, userNumber, onGameOver])
 
@@ -59,6 +61,9 @@ const GameScreen = ({ userNumber, onGameOver }) => {
     setCurrentGuess(newRndNumber)
     setGuessRounds((prevGuessRounds) => [newRndNumber, ...prevGuessRounds])
   }
+
+  const guessRoundsListLength = guessRounds.length
+
   return (
     <View style={styles.screen}>
       <Title>Opponent's Guess</Title>
@@ -82,13 +87,18 @@ const GameScreen = ({ userNumber, onGameOver }) => {
           </View>
         </Card>
       </View>
-      <View>
+      <View style={styles.listContainer}>
         {/* {guessRounds.map((guessRound) => (
           <Text key={guessRound}>{guessRound}</Text>
         ))} */}
         <FlatList
           data={guessRounds}
-          renderItem={(itemData) => <Text>{itemData.item}</Text>}
+          renderItem={(itemData) => (
+            <GuessLogItem
+              roundNumber={guessRoundsListLength - itemData.index}
+              guess={itemData.item}
+            />
+          )}
           keyExtractor={(item) => item}
         />
       </View>
@@ -111,5 +121,9 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     flex: 1,
+  },
+  listContainer: {
+    flex: 1,
+    padding: 16,
   },
 })
